ceb8b8be1e0effb1220c646282aa515037258da7
Kathryn Lewis <kathryn.lewis@armory.io>
2021-01-29 13:38:40 -0800

    chore(build): merge latest upstream (#1)
    Co-authored-by: root <root@6a807b439d68>
    Co-authored-by: root <root@87c6cb8f1126>
    Co-authored-by: root <root@048626cb7ef2>
    Co-authored-by: root <root@4bab6a330c11>
    Co-authored-by: root <root@534ce9888ac0>
    Co-authored-by: root <root@167ad51ad4f2>
    Co-authored-by: root <root@07aa874cb083>
    Co-authored-by: root <root@fa990adebdf5>
    Co-authored-by: root <root@ec7bd04f30f1>
    Co-authored-by: root <root@07935c2d2f7e>
    Co-authored-by: root <root@bfd54d97d9e4>
    Co-authored-by: root <root@903b3fc96ec5>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: root <root@b207540a8467>
    Co-authored-by: root <root@37fd038da766>
    Co-authored-by: root <root@cd7bcd8dd69a>
    Co-authored-by: root <root@4181cde18944>
    Co-authored-by: root <root@e8550e2f1fc9>
    Co-authored-by: root <root@1c964967be5d>
    Co-authored-by: root <root@3d7a4776ad28>
    Co-authored-by: root <root@ad731cfb1014>
    Co-authored-by: root <root@9641193bdc80>
    Co-authored-by: root <root@96ca2b2c6285>
    Co-authored-by: root <root@82abca9e29b6>
    Co-authored-by: root <root@28bd9d7e8d49>
    Co-authored-by: root <root@af3522b9b644>
    Co-authored-by: root <root@6a7b3894974d>
    Co-authored-by: root <root@d7e7980769a5>
    Co-authored-by: root <root@807cc584393a>
    Co-authored-by: root <root@b272e1bb32f7>
    Co-authored-by: root <root@f9377741a2e4>
    Co-authored-by: root <root@a88a9ef8bf72>
    Co-authored-by: root <root@5900c8b382c3>
    Co-authored-by: root <root@877703f80d77>
    Co-authored-by: root <root@c51049d641bc>
    Co-authored-by: root <root@893456aa42c8>
    Co-authored-by: root <root@e9b0b38e4e5f>
    Co-authored-by: root <root@9c1f95bcaee1>
    Co-authored-by: root <root@73fd689d0395>
    Co-authored-by: root <root@dcc11ef833df>
    Co-authored-by: root <root@9a9d052c27fd>
    Co-authored-by: root <root@7b52afe39ecb>
    Co-authored-by: root <root@d456b8dff913>
    Co-authored-by: root <root@e00e7b323342>
    Co-authored-by: root <root@0d0260787168>
    Co-authored-by: root <root@292101d0d717>
    Co-authored-by: root <root@3814c4b6ed6a>
    Co-authored-by: root <root@a467bf5216d2>
    Co-authored-by: root <root@2f7db28adec5>
    Co-authored-by: root <root@ae58f56d23c5>
    Co-authored-by: root <root@dd10b3e8c985>
    Co-authored-by: spinnakerbot <spinbot@spinnaker.io>
    Co-authored-by: Cameron Fieber <cfieber@netflix.com>
    Co-authored-by: Edgar Garcia <63310723+edgarulg@users.noreply.github.com>
    Co-authored-by: Eric Zimanyi <ezimanyi@google.com>
    Co-authored-by: Jeffrey Coe <jeffreycoe@users.noreply.github.com>
    Co-authored-by: Chris Smalley <csmalley@netflix.com>
    Co-authored-by: Sairam Rekapalli <srekapalli@netflix.com>
    Co-authored-by: Daniel Peach <daniel.peach@armory.io>
    Co-authored-by: Michael Plump <plumpy@google.com>
    Co-authored-by: Fernando Freire <dogonthehorizon@gmail.com>
    Co-authored-by: Adam Jordens <adam@jordens.org>
    Co-authored-by: Rob Fletcher <robfletcher@users.noreply.github.com>

--- front50-web/src/main/java/com/netflix/spinnaker/front50/config/Front50WebConfig.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/config/Front50WebConfig.java
@@ -17,8 +17,8 @@
 package com.netflix.spinnaker.front50.config;
 
 import com.netflix.spectator.api.Registry;
+import com.netflix.spinnaker.config.PluginsAutoConfiguration;
 import com.netflix.spinnaker.fiat.shared.EnableFiatAutoConfig;
-import com.netflix.spinnaker.fiat.shared.FiatAccessDeniedExceptionHandler;
 import com.netflix.spinnaker.fiat.shared.FiatClientConfigurationProperties;
 import com.netflix.spinnaker.fiat.shared.FiatStatus;
 import com.netflix.spinnaker.filters.AuthenticatedRequestFilter;
@@ -43,6 +43,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -54,6 +55,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 @EnableFiatAutoConfig
 @EnableScheduling
+@Import({PluginsAutoConfiguration.class})
 @EnableConfigurationProperties(StorageServiceConfigurationProperties.class)
 public class Front50WebConfig extends WebMvcConfigurerAdapter {
 
@@ -136,11 +138,6 @@ public class Front50WebConfig extends WebMvcConfigurerAdapter {
   public ItemDAOHealthIndicator deliveryRepositoryHealthIndicator(
       DeliveryRepository deliveryRepository, TaskScheduler taskScheduler) {
     return new ItemDAOHealthIndicator(deliveryRepository, taskScheduler);
-  }
-
-  @Bean
-  public FiatAccessDeniedExceptionHandler fiatAccessDeniedExceptionHandler() {
-    return new FiatAccessDeniedExceptionHandler();
   }
 
   @Bean
--- front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
@@ -30,6 +30,7 @@ import com.netflix.spinnaker.front50.model.pipeline.Pipeline;
 import com.netflix.spinnaker.front50.model.pipeline.PipelineDAO;
 import com.netflix.spinnaker.front50.model.pipeline.PipelineTemplateDAO;
 import com.netflix.spinnaker.front50.model.pipeline.TemplateConfiguration;
+import com.netflix.spinnaker.front50.model.pipeline.Trigger;
 import com.netflix.spinnaker.front50.model.pipeline.V2TemplateConfiguration;
 import com.netflix.spinnaker.front50.validator.GenericValidationErrors;
 import com.netflix.spinnaker.front50.validator.PipelineValidator;
@@ -42,6 +43,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
@@ -156,9 +158,11 @@ public class PipelineController {
     if (Strings.isNullOrEmpty(pipeline.getId())
         || (boolean) pipeline.getOrDefault("regenerateCronTriggerIds", false)) {
       // ensure that cron triggers are assigned a unique identifier for new pipelines
-      pipeline.getTriggers().stream()
+      Collection<Trigger> triggers = pipeline.getTriggers();
+      triggers.stream()
           .filter(it -> "cron".equals(it.getType()))
           .forEach(it -> it.put("id", UUID.randomUUID().toString()));
+      pipeline.setTriggers(triggers);
     }
 
     return pipelineDAO.create(pipeline.getId(), pipeline);
@@ -224,8 +228,7 @@ public class PipelineController {
    */
   private void validatePipeline(final Pipeline pipeline, Boolean staleCheck) {
     // Pipelines must have an application and a name
-    if (Strings.isNullOrEmpty(pipeline.getApplication())
-        || Strings.isNullOrEmpty(pipeline.getName())) {
+    if (StringUtils.isAnyBlank(pipeline.getApplication(), pipeline.getName())) {
       throw new InvalidEntityException("A pipeline requires name and application fields");
     }
 
@@ -318,12 +321,20 @@ public class PipelineController {
 
   private static Pipeline ensureCronTriggersHaveIdentifier(Pipeline pipeline) {
     // ensure that all cron triggers have an assigned identifier
-    pipeline.getTriggers().stream()
+    Collection<Trigger> triggers = pipeline.getTriggers();
+    triggers.stream()
         .filter(it -> "cron".equalsIgnoreCase(it.getType()))
         .forEach(
-            it ->
-                it.put(
-                    "id", Optional.ofNullable(it.get("id")).orElse(UUID.randomUUID().toString())));
+            it -> {
+              String triggerId = (String) it.get("id");
+              if (triggerId == null || triggerId.isEmpty()) {
+                triggerId = UUID.randomUUID().toString();
+              }
+
+              it.put("id", triggerId);
+            });
+    pipeline.setTriggers(triggers);
+
     return pipeline;
   }
 }
